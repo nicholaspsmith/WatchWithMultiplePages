6e622c280d29b7bdd3e42ee471978d01a6e45e73
Nick P Smith <npsmith1990@gmail.com>
2016-02-10 19:34:02 -0600

    Initial Commit

---
+++ Apple Watch App WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,34 @@
+//
+//  InterfaceController.swift
+//  Apple Watch App WatchKit Extension
+//
+//  Created by Rob Percival on 15/12/2014.
+//  Copyright (c) 2014 Appfish. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+        NSLog("%@ init", self)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+}
